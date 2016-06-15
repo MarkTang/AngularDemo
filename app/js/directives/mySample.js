c02b8a1a20afec79fb6876da22b8b674aa8e6126
Mark Wang <marktangwao@foxmail.com>
2016-06-15 11:40:23 +0800

    Add codes which isolate directive scope.

--- app/js/directives/mySample.js
+++ app/js/directives/mySample.js
@@ -3,12 +3,15 @@
 eventsApp.directive('mySample', function($compile) {
 
     return {
-        restrict: "E",
+        restrict: "ECA",
         // link: function(scope, element, attrs, controller) {
         //     var markup = "<input type='text' ng-model='sampleData' /> <br /> {{sampleData}} <br />";
         //     angular.element(element).html($compile(markup)(scope));
         // }
-        template: "<input type='text' ng-model='sampleData' /> <br /> {{sampleData}} <br />"
+        template: "<input type='text' ng-model='sampleData' /> {{sampleData}} <br />",
+        scope: {
+
+        }
     };
 
 });
